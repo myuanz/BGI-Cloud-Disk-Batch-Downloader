a021c7c1b331f9b10ec53965e841bfe51c1c873f
myuan <zhengmy@ion.ac.cn>
2022-09-09 09:33:38 +0800

    添加提取码

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,11 @@ struct Args {
     /// export to
     #[clap(short, long, value_parser, default_value = "./download-from-bgi")]
     export_root: String,
+
+    /// extraction code
+    #[clap(short, long, value_parser, default_value = "")]
+    code: String,
+
 }
 
 #[derive(Debug)]
@@ -28,7 +33,7 @@ struct DownloadArgs {
 
 #[async_recursion]
 async fn find_all_files(
-    sess: &reqwest::Client, base_rows: &Vec<Value>, share_event_id: &str, tab: usize
+    sess: &reqwest::Client, base_rows: &Vec<Value>, share_event_id: &str, code: &str, tab: usize
 ) -> Option<Vec<DownloadArgs>> {
     let mut res: Vec<DownloadArgs> = vec![];
 
@@ -44,13 +49,14 @@ async fn find_all_files(
                     ("keyword", ""),
                     ("shareEventId", share_event_id),
                     ("nodeId", &row["id"]),
-                    ("code", ""),
+                    ("code", code),
                 )).send().await.unwrap().json::<Value>().await.unwrap();
 
             res.append(&mut find_all_files(
                 sess,
                 rows_res["body"]["rows"].as_array().unwrap(),
                 share_event_id,
+                code, 
                 tab + 1
             ).await?)
         } else {
@@ -76,7 +82,7 @@ fn check_os_and_aria2c() -> String {
         .output();
     match version {
         Ok(v) => {
-            if !std::str::from_utf8(&v.stdout).unwrap().contains("aria2 version") {
+            if !std::str::from_utf8(&v.stdout).unwrap().contains("Tatsuhiro Tsujikawa") {
                 panic!("aria2 version not found");
             }
         }
@@ -94,6 +100,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
     let download_url: String = args.url;
     let export_root: String = args.export_root;
+    let code: String = args.code;
 
 
     let sess = reqwest::Client::new();
@@ -111,18 +118,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             ("shareEventId", share_event_id),
             ("pageNumber", 1),
             ("pageSize", 500),
-            ("code", ""),
+            ("code", &code),
         )).send().await?.json::<Value>().await?;
 
     let base_rows: &Vec<Value> = &base_dir_json["body"]["rows"].as_array().unwrap();
-    let all_files: Vec<DownloadArgs> = find_all_files(&sess, base_rows, share_event_id,0).await.unwrap();
+    let all_files: Vec<DownloadArgs> = find_all_files(&sess, base_rows, share_event_id, &code, 0).await.unwrap();
 
     let base_dir = std::format!("{}/{}", export_root, Local::now().format("%Y%m%d"));
     let download_list_file = fs::File::create("./download_list.txt").unwrap();
     let mut download_list_file = LineWriter::new(download_list_file);
 
     for file in all_files {
-        download_list_file.write_all(format!("https://pan.genomics.cn/ucdisk/api/2.0/share/link/download?shareEventId={}&nodeId={}&code=\n", share_event_id, file.row_id).as_bytes()).unwrap();
+        download_list_file.write_all(format!("https://pan.genomics.cn/ucdisk/api/2.0/share/link/download?shareEventId={}&nodeId={}&code={}\n", share_event_id, file.row_id, code).as_bytes()).unwrap();
         download_list_file.write_all(format!("\tout={}\n", file.path).as_bytes()).unwrap();
     }
     println!("download to {}", base_dir);
